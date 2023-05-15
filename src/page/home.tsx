40e3049a40e42c1ea9244e46eda82d5851a3e2ba
412 <85489519+laybacksound96@users.noreply.github.com>
2023-05-15 11:33:30 +0900

    footer 추가

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as DiscordIcon } from "../discord-icon.svg";
 const Header = styled.header`
   display: flex;
   justify-content: start;
+  height: 750px;
 `;
 const Section = styled.section`
   margin-top: 200px;
@@ -13,7 +14,7 @@ const Section = styled.section`
     align-items: start;
   }
   h1 {
-    font-size: 130px;
+    font-size: 160px;
     margin-bottom: 10px;
   }
   span {
@@ -57,25 +58,81 @@ const LoginDiscordBtn = styled.button`
   }
 `;
 
+const Article = styled.article`
+  height: 3000px;
+  background-color: ${(props) => props.theme.bgColor};
+  padding-left: 150px;
+  padding-top: 100px;
+  p {
+    width: 600px;
+    font-size: 20px;
+  }
+  div {
+    display: flex;
+    justify-content: start;
+  }
+  h1 {
+    font-size: 60px;
+    color: ${(props) => props.theme.accentColor};
+    margin-bottom: 15px;
+  }
+`;
+
+const ImgFake = styled.div`
+  margin-left: 200px;
+  width: 300px;
+  height: auto;
+  background-color: #000000;
+`;
+const Footer = styled.footer`
+  background-color: #181924;
+  color: ${(props) => props.theme.accentColor};
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 function home() {
   const loginHandlerDiscord = (event: React.MouseEvent) => {
     event.preventDefault();
     console.log("인증콜백 여기에 넣자");
   };
   return (
-    <Header>
-      <Section>
-        <h1>Check.loa</h1>
+    <>
+      <Header>
+        <Section>
+          <h1>Check.loa</h1>
+          <div>
+            <span>간편하고 똑똑한 일정관리 앱</span>
+            <LoginDiscordBtn onClick={loginHandlerDiscord}>
+              <DiscordIcon />
+              <p>Discord로 로그인</p>
+            </LoginDiscordBtn>
+            <a href="/">로그인 없이 시작</a>
+          </div>
+        </Section>
+      </Header>
+      <Article>
         <div>
-          <span>간편하고 똑똑한 일정관리 앱</span>
-          <LoginDiscordBtn onClick={loginHandlerDiscord}>
-            <DiscordIcon />
-            <p>Discord로 로그인</p>
-          </LoginDiscordBtn>
-          <a href="/">로그인 없이 시작</a>
+          <p>
+            <h1>Lorem Ipsum</h1>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <ImgFake />
         </div>
-      </Section>
-    </Header>
+      </Article>
+      <Footer>
+        <p>Contact us : 412#4615</p>
+      </Footer>
+    </>
   );
 }
 
