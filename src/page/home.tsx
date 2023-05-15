4f721f999179e44146e4548f0ac3f76c934a72ba
412 <85489519+laybacksound96@users.noreply.github.com>
2023-05-15 10:02:08 +0900

    테마 지정

---
+++ src/page/home.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function home() {
+  return <Container />;
+}
+
+export default home;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2b2d42",
+  subColor: "#8d99ae",
+  accentColor: "#edf2f4",
+};
