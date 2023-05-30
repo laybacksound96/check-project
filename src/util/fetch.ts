5197204ff0e4bc8fda9d98d79ef413870bc8130d
412 <laybacksound96@gmail.com>
2023-05-30 10:00:11 +0900

    엔드포인트 변경

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export async function fetchLogin(): Promise<string> {
-  const response = await axios.get("http://localhost:4000/login");
+  const response = await axios.get("http://localhost:4000/user/login");
 
   return response.data.loginUrl;
 }
 
 export async function fetchSearchAccount(): Promise<object> {
-  const response = await axios.get("http://localhost:4000/character");
+  const response = await axios.get("http://localhost:4000/user/character");
   console.log(response);
   return {};
 }
