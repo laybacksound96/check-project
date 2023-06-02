1558c1f38a43157b671bc2c3314e7b4d5523923c
412 <laybacksound96@gmail.com>
2023-06-02 15:06:05 +0900

    군단장 db변경

--- src/components/Ui/Modal/ModalContents/functions/makeActivatedAndDifficulty.ts
+++ src/components/Ui/Modal/ModalContents/functions/makeActivatedAndDifficulty.ts
@@ -1,9 +1,16 @@
+import commander from "./commander.json";
 export const makeAvailableDifficulty = (
   level: number,
-  content: string,
+  content:
+    | "발탄"
+    | "비아키스"
+    | "쿠크세이튼"
+    | "아브렐슈드"
+    | "일리아칸"
+    | "아르고스",
   gateNumber: number
 ): { ["isActivated"]: boolean; ["Difficulty"]?: "normal" | "hard" } => {
-  return {};
+  const commanderState = 
 };
 
 export default makeAvailableDifficulty;
