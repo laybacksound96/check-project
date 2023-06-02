8e56503436e60f2730d922d7585a63092b217df9
412 <laybacksound96@gmail.com>
2023-06-02 14:26:26 +0900

    makeConfigObject 리팩토링

---
+++ src/components/Ui/Modal/ModalContents/functions/makeActivatedAndDifficulty.ts
@@ -0,0 +1,9 @@
+export const makeAvailableDifficulty = (
+  level: number,
+  content: string,
+  gateNumber: number
+): { ["isActivated"]: boolean; ["Difficulty"]?: "normal" | "hard" } => {
+  return {};
+};
+
+export default makeAvailableDifficulty;
--- src/components/Ui/Modal/ModalContents/functions/makeConfigObject.ts
+++ src/components/Ui/Modal/ModalContents/functions/makeConfigObject.ts
@@ -1,14 +1,34 @@
-import { IConfigObject, IContentName } from "../../../../../atoms";
+import { IConfigObject, IContentName, IGates } from "../../../../../atoms";
 import CountGates from "./CountGates";
+import makeActivatedAndDifficulty from "./makeActivatedAndDifficulty";
 
 const makeConfigObject = (
   level: number,
   content: string,
   type: string
 ): IContentName => {
-  const result: IContentName = {};
-  const gateCount = CountGates(content);
-  return result;
+  const ConfigObject: IConfigObject = {
+    isCleared: false,
+    isVisible: true,
+  };
+  if (type === "default") {
+    const gateCount = CountGates(content);
+    for (let gateNumber = 1; gateNumber > gateCount; gateNumber++) {
+      const { isActivated, Difficulty } = makeActivatedAndDifficulty(
+        level,
+        content,
+        gateNumber
+      );
+      const Gate: IGates = {
+        Gate_No: gateNumber,
+        isActivated,
+        Difficulty,
+      };
+      ConfigObject.Gates?.push(Gate);
+    }
+  }
+  const ContentName: IContentName = { [`${content}`]: ConfigObject };
+  return ContentName;
 };
 
 export default makeConfigObject;
