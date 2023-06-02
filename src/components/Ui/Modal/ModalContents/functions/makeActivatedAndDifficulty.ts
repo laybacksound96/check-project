236905ff2849c72811aaa048fd753b6fc3682f3a
412 <laybacksound96@gmail.com>
2023-06-02 15:59:59 +0900

    makeAvailableDifficulty 수정

--- src/components/Ui/Modal/ModalContents/functions/makeActivatedAndDifficulty.ts
+++ src/components/Ui/Modal/ModalContents/functions/makeActivatedAndDifficulty.ts
@@ -1,4 +1,19 @@
 import commander from "./commander.json";
+interface resultObject {
+  ["isActivated"]: boolean;
+  ["Difficulty"]?: string;
+}
+interface commanderInfo {
+  gateNumber: number;
+  level: number;
+  gold: number;
+}
+interface commaderNames {
+  [difficulty: string]: commanderInfo;
+}
+interface IData {
+  [name: string]: commaderNames[];
+}
 export const makeAvailableDifficulty = (
   level: number,
   content:
@@ -9,8 +24,20 @@ export const makeAvailableDifficulty = (
     | "일리아칸"
     | "아르고스",
   gateNumber: number
-): { ["isActivated"]: boolean; ["Difficulty"]?: "normal" | "hard" } => {
-  const commanderState = 
+): resultObject => {
+  const resultDifficulty: resultObject = {
+    isActivated: false,
+    Difficulty: "normal",
+  };
+  const commanderData: IData = commander;
+  const gateData = commanderData[content][gateNumber - 1];
+  for (let difficulty in gateData) {
+    if (level >= gateData[difficulty].level) {
+      resultDifficulty.isActivated = true;
+      resultDifficulty.Difficulty = difficulty;
+    }
+  }
+  return resultDifficulty;
 };
 
 export default makeAvailableDifficulty;
