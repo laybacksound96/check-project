6ce66be9fec8687e1bf366325da4f064399fd1b9
412 <laybacksound96@gmail.com>
2023-06-04 02:56:25 +0900

    수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import App2 from "./asdsd";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App2 />);
+root.render(<App />);
