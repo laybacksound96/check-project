b2dd81463a07f19a62e4dbab206240dc186a4157
412 <laybacksound96@gmail.com>
2023-06-09 14:09:28 +0900

    카양겔, 상아탑 추가

--- src/components/DashBoard/Functions/getRandomPastelColor.ts
+++ src/components/DashBoard/Functions/getRandomPastelColor.ts
@@ -1,8 +1,8 @@
 function getRandomPastelColor(ContentName: string): string {
-  const minLightness = 30; // 최소 명도 (0 ~ 100)
-  const maxLightness = 60; // 최대 명도 (0 ~ 100)
-  const minSaturation = 20; // 최소 채도 (0 ~ 100)
-  const maxSaturation = 40; // 최대 채도 (0 ~ 100)
+  let minSaturation = 20; // 최소 채도 (0 ~ 100)
+  let maxSaturation = 40; // 최대 채도 (0 ~ 100)
+  let minLightness = 30; // 최소 명도 (0 ~ 100)
+  let maxLightness = 60; // 최대 명도 (0 ~ 100)
 
   let hue = Math.floor(Math.random() * 360); // 0 ~ 359 사이의 랜덤한 색상
   if (ContentName === "아브렐슈드") hue = 240;
@@ -10,6 +10,12 @@ function getRandomPastelColor(ContentName: string): string {
   if (ContentName === "비아키스") hue = 300;
   if (ContentName === "쿠크세이튼") hue = 0;
   if (ContentName === "일리아칸") hue = 134;
+  if (ContentName === "아르고스") {
+    hue = 203;
+    maxSaturation = 25;
+    minLightness = 20;
+    maxLightness = 30;
+  }
 
   const saturation = Math.floor(
     Math.random() * (maxSaturation - minSaturation + 1) + minSaturation
