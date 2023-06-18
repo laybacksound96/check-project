f2f41d3353e41adb919f5c1b504393e969653139
412 <laybacksound96@gmail.com>
2023-06-18 23:08:31 +0900

    코드이름변경

--- src/components/DashBoard/DragAccounts/DragAccounts.tsx
+++ src/components/DashBoard/DragAccounts/DragAccounts.tsx
@@ -11,7 +11,7 @@ import { useRecoilState, useSetRecoilState } from "recoil";
 import { AccountOrder } from "../../../atoms/order";
 import { ModalEnum, ModalState } from "../../../atoms/modal";
 import { AxisLocker } from "../Functions/AxisLocker";
-import DragCharacters from "./DragCharacters";
+import DragCharacters from "./DragCharactersAndColumn";
 
 const DragBoxStyle = styled.div`
   width: 100%;
