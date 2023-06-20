d883b3d27f46cade80ee90f168d0cc5092f32066
412 <laybacksound96@gmail.com>
2023-06-20 17:30:09 +0900

    configContent 캐릭터 표시설정

--- src/components/Ui/Modal/ModalContents/ConfigContent.tsx
+++ src/components/Ui/Modal/ModalContents/ConfigContent.tsx
@@ -1,20 +1,66 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { ModalState } from "../../../../atoms/modal";
 import { AccountState } from "../../../../atoms/atoms";
 import { Container, GridContainer } from "./ConfigAccount";
-import { CardHeader } from "./components/ContentCard";
-import { useState } from "react";
 import styled from "styled-components";
-interface IStyel {}
-const NameCard = styled.div`
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+interface IStyel {
+  isVisible: boolean;
+}
+const Character = styled.div<IStyel>`
+  opacity: ${(props) => (props.isVisible ? "100%" : "20%")};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 200px;
-  height: 60px;
-  background-color: ${(props) => props.theme.Color_1};
+  padding-left: 5px;
+  font-size: 17px;
+  height: 70px;
   border-radius: 5px;
-  margin: 10px 0;
-  font-size: 20px;
+  background-color: rgba(100, 100, 100, 0.1);
+  margin-bottom: 10px;
+  &:hover {
+    background-color: rgba(100, 100, 100, 0.5);
+    transition: ease-in-out 0.1s;
+    svg {
+      opacity: 70%;
+    }
+  }
+
+  svg {
+    opacity: 20%;
+    border-radius: 10px;
+    font-size: 30px;
+    padding: 10px 10px;
+  }
+  svg:hover {
+    opacity: 100%;
+    background-color: rgba(100, 100, 100, 0.7);
+  }
 `;
-const ContentList = styled.div<IStyel>`
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  height: 100%;
+  margin-right: 10px;
+`;
+const NameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  span {
+    font-size: 0.9rem;
+    opacity: 40%;
+    color: ${(props) => props.theme.TextColor_A};
+    &:nth-child(2) {
+      font-size: 0.85rem;
+    }
+  }
+`;
+const ContentList = styled.div`
   display: flex;
   flex-direction: column;
   background-color: ${(props) => props.theme.Color_4};
@@ -27,30 +73,65 @@ const ContentList = styled.div<IStyel>`
     font-size: 30px;
     margin-bottom: 10px;
   }
-
   transition: opacity 0.3s ease-in-out;
+  header {
+    font-size: 20px;
+    margin: 5px 0 10px 0;
+    svg {
+      margin-right: 5px;
+    }
+  }
 `;
 export const ConfigContent = () => {
   const {
     modalProp: { AccountName },
   } = useRecoilValue(ModalState);
-  const characterState = useRecoilValue(AccountState)[AccountName];
-  const [isHovered, setIsHovered] = useState(false);
+  const [accountState, setAccountState] = useRecoilState(AccountState);
+
+  const handleVisible = (CharacterName: string) => {
+    setAccountState((prev) => {
+      const currentVisible = prev[AccountName][CharacterName].isVisible;
+      const copiedPrev = {
+        ...prev,
+        [AccountName]: {
+          ...prev[AccountName],
+          [CharacterName]: {
+            ...prev[AccountName][CharacterName],
+            isVisible: !currentVisible,
+          },
+        },
+      };
+
+      return copiedPrev;
+    });
+  };
   return (
     <Container>
       <GridContainer>
-        <ContentList
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <CardHeader>
-            <h1>asdsad</h1>
-          </CardHeader>
-          <div>
-            {Object.keys(characterState).map((character) => (
-              <NameCard>{character}</NameCard>
-            ))}
-          </div>
+        <ContentList>
+          <header>
+            <FontAwesomeIcon icon={faGear} />
+            <span>캐릭터 표시 설정</span>
+          </header>
+          {Object.keys(accountState[AccountName]).map((character) => {
+            const { CharacterClassName, ItemMaxLevel, isVisible } =
+              accountState[AccountName][character];
+            return (
+              <Character key={character} isVisible={isVisible}>
+                <NameContainer>
+                  {character}
+                  <span>{CharacterClassName}</span>
+                  <span>Lv {ItemMaxLevel}</span>
+                </NameContainer>
+                <ButtonContainer>
+                  <FontAwesomeIcon
+                    onClick={() => handleVisible(character)}
+                    icon={isVisible ? faEye : faEyeSlash}
+                  />
+                </ButtonContainer>
+              </Character>
+            );
+          })}
         </ContentList>
       </GridContainer>
     </Container>
