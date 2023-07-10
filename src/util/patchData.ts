b39e1d4a5509793584d8a7423bf76a9a0bfd087e
412 <laybacksound96@gmail.com>
2023-07-11 00:38:38 +0900

    patchData 수정

--- src/util/patchData.ts
+++ src/util/patchData.ts
@@ -3,11 +3,13 @@ import { IData } from "../CustomHooks/Login/useSetAllAtoms";
 let lastCallTimeout: any = null;
 
 function patchData(data: IData) {
+  const { accountOrder } = data;
   if (lastCallTimeout) {
     clearTimeout(lastCallTimeout);
   }
 
   lastCallTimeout = setTimeout(() => {
+    if (accountOrder.length <= 0) return;
     console.log("Final A function call");
     lastCallTimeout = null;
   }, 5000);
