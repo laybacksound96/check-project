ef4fb27eec7d30e4332921e7a6bb1f3f733bc1b2
412 <laybacksound96@gmail.com>
2023-07-18 22:01:05 +0900

    home ui 조정

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,5 @@
 import { Navigate, useRouteLoaderData } from "react-router-dom";
-import { Main, LoginDiscordBtn, Article, ImgFake } from "../Style/Home";
+import { Main, LoginDiscordBtn } from "../Style/Home";
 import { ReactComponent as DiscordIcon } from "../icons/discord-icon.svg";
 import { fetchLogin } from "../util/fetch";
 import { loadToken } from "../util/auth";
@@ -24,24 +24,6 @@ function Home() {
         </LoginDiscordBtn>
         <a href="/board/GUEST">로그인 없이 시작</a>
       </section>
-      <div style={{ height: "300px" }}></div>
-      <Article>
-        <h1>Lorem Ipsum</h1>
-        <div>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <ImgFake />
-        </div>
-      </Article>
     </Main>
   );
 }
