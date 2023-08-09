f08cef8bf8efb7b151fecffee8d48146a613ba39
412 <laybacksound96@gmail.com>
2023-08-10 08:18:05 +0900

    contentsOrder 마무리

--- src/components/Ui/Modal/ModalContents/functions/AddAccount/makeAccount.ts
+++ src/components/Ui/Modal/ModalContents/functions/AddAccount/makeAccount.ts
@@ -1,16 +1,54 @@
-import { IAccounts } from "../../../../../../atoms/data";
+import { IAccounts, ICharacters } from "../../../../../../atoms/data";
+import { CommanderData } from "../../../../../../json/CommanderData";
 import { IFetchedCharacter } from "../../AddAccount";
-import makeCharacter from "./makeCharacter";
-import makeContent from "./makeContent";
+import calculateGold from "./calculateGold";
+import makeGates from "./makeGates";
 
 const makeDataResult = (data: IFetchedCharacter[]): IAccounts => {
-  const { characterOrder, characters: preCharacters } = makeCharacter(data);
-  const { contentsOrder, characters } = makeContent(preCharacters);
-  const result: IAccounts = {
+  const characterOrder: string[] = [];
+  const contentsOrder: string[] = [];
+  const characters: ICharacters[] = [];
+  const commanderData = CommanderData;
+  for (let i in data) {
+    const {
+      ServerName: serverName,
+      ItemMaxLevel,
+      CharacterName: characterName,
+      CharacterClassName: className,
+    } = data[i];
+    characters.push({
+      characterName,
+      className,
+      level: parseInt(ItemMaxLevel.replace(",", "")),
+      serverName,
+      contents: [],
+    });
+    if (+i < 6) characterOrder.push(characterName);
+  }
+
+  for (let i in characters) {
+    const { level, contents, characterName } = characters[i];
+    const goldArray = calculateGold(level, CommanderData);
+    if (characterOrder.includes(characterName)) {
+      goldArray.forEach((elem) => {
+        if (!contentsOrder.includes(elem)) contentsOrder.push(elem);
+      });
+      contentsOrder.reverse();
+    }
+    for (let j in commanderData) {
+      const { contentName, gates } = commanderData[j];
+      contents.push({
+        contentName,
+        isVisible: goldArray.includes(contentName),
+        isGoldContents: goldArray.includes(contentName),
+        gates: makeGates(level, gates),
+      });
+    }
+  }
+  return {
     characterOrder,
     characters,
     contentsOrder,
   };
-  return result;
 };
 export default makeDataResult;
